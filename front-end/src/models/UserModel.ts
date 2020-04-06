cf3339066e67e79ad22585597c875fa139def4df
Bejong Yang <bejongyang@gmail.com>
2020-04-05 22:25:30 -0500

    added user models and services

---
+++ front-end/src/models/UserModel.ts
@@ -0,0 +1,4 @@
+export class UserModel {
+    constructor(firstName: string, lastName: string, primaryAddress: string, secondaryAddress: string, city: string,
+        tate: string, zip: number, email: string, phone:number, password:string) {}
+}
---
+++ front-end/src/services/LoginService.ts
@@ -0,0 +1,16 @@
+import { UserModel } from "../models/UserModel";
+
+export default class LoginService {
+
+    public CreateNewUser(user: UserModel): any {
+        return null;
+    }
+
+    public ValidateNewUser(): any {
+        return null;
+    }
+
+    public Login(user: UserModel): any {
+        return null;
+    }
+}
