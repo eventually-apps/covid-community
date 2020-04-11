c5c685f5c10eeb185a5f87e4183615df95f16bdc
Chad Smith <chadsmith4@my.unt.edu>
2020-04-11 18:23:49 -0500

    login submit on enter

--- front-end/src/views/Login.vue
+++ front-end/src/views/Login.vue
@@ -3,7 +3,7 @@
     <v-container class="fill-height" fluid>
       <v-row align="center" justify="center">
         <v-col cols="12" sm="8" md="4">
-          <v-form>
+          <v-form @keypress.native.enter="Login">
             <v-text-field label="Login" v-model="username" name="login" type="text" />
 
             <v-text-field
@@ -15,7 +15,7 @@
             />
           </v-form>
           <v-row>
-            <v-btn color="primary" @click="Login">Login</v-btn>
+            <v-btn type="submit" color="primary" @click="Login">Login</v-btn>
           </v-row>
           <v-row style="margin-top: 10px;">
             <v-btn class="signUpBtn" color="primary" :to="{path: '/SignUp'}">Sign Up</v-btn>
